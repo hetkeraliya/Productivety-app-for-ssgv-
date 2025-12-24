97f625008675886916288e0d07ccbd6edfaf565d
hetkeraliya <jalpa5398@gmail.com>
2025-12-24 18:18:18 +0530

    Add .jsx extension to App import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // We remove the .jsx extension to help Vite resolve it
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
