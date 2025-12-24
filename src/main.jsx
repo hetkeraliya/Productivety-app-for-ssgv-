7347567b1900cc56e87745306ecd26f69b538573
hetkeraliya <jalpa5398@gmail.com>
2025-12-24 18:14:41 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App' // We remove the .jsx extension to help Vite resolve it
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+
